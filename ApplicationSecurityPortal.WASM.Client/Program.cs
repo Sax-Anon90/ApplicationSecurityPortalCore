e161fd61030f203f91aa8c8ad99b3dc7600413f6
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2024-03-03 16:47:26 +0200

    Add: Radzen UI library

--- ApplicationSecurityPortal.WASM.Client/Program.cs
+++ ApplicationSecurityPortal.WASM.Client/Program.cs
@@ -1,11 +1,13 @@
 using ApplicationSecurityPortal.WASM.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
